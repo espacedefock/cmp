14ff7989faa4172e3fb2a872fd02605244e509ad
VirginieGodfrin <virginiegodfrin@gmail.com>
2021-10-18 06:47:14 +0200

    68.Binding to Custom Events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,4 +16,20 @@ export class AppComponent {
 
   ngOnInit(): void {
   }
+
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBulePrintAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 }
